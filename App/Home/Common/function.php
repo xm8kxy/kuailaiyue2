bb051d2cfe5d13aecd2d0270d85498aae3313fe7
xm <306088569@qq.com>
2018-01-09 08:27:49 +0800

    开始写订单

--- App/Home/Common/function.php
+++ App/Home/Common/function.php
@@ -131,8 +131,30 @@ function xm_gf($table='XmTab',$field='*',$where=null,$limit=null)
     return  $datas;
     exit;
 }
-
-
+//唯一邀请码
+function make_coupon_card() {
+    $code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $rand = $code[rand(0,25)]
+        .strtoupper(dechex(date('m')))
+        .date('d').substr(time(),-5)
+        .substr(microtime(),2,5)
+        .sprintf('%02d',rand(0,99));
+    for(
+        $a = md5( $rand, true ),
+        $s = '0123456789ABCDEFGHIJKLMNOPQRSTUV',
+        $d = '',
+        $f = 0;
+        $f < 8;
+        $g = ord( $a[ $f ] ),
+        $d .= $s[ ( $g ^ ord( $a[ $f + 8 ] ) ) - $g & 0x1F ],
+        $f++
+    );
+    return $d;
+}
+//生成唯一订单号
+function build_order_no(){
+    return date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
+}
 //------------------------------------------
 
 /*
--- App/Home/Controller/ApiController.class.php
+++ App/Home/Controller/ApiController.class.php
@@ -31,10 +31,31 @@ class ApiController extends ComController
         if ($xycs == '') {returnApiError( '校验码必须！');}
     //    if ($verify!=$xycs){returnApiError( '非法数据');}
 
+//不用验证是方法
+        $no_dr=array();
+        $no_dr[]='is_jwt';//jwt验证
+        $no_dr[]='register';//注册
+        $no_dr[]='ziyezs';//选择性别和职业展示页
+        $no_dr[]='ziyeqr'; //确认选择性别和职业
+        $no_dr[]='xmandage'; //完善资料
+        $no_dr[]='login'; //登入
+        $no_dr[]='index'; //
+
+        if(!in_array(ACTION_NAME, $no_dr)){
+            $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+            $str = isset($_POST['token']) ? trim($_POST['token']) : '';
+            $key =  $this->key_xm;
+            if ($user_id == '') { returnApiError( '用户id必须');}
+            if ($str == '') { returnApiError( 'token必须');}
+            if ($key == '') { returnApiError( 'key必须');}
+            $this->is_jwt($str,$key,1,$user_id);
+        }
     }
+
+
     public function index()
     {
-
+        echo build_order_no();exit;
    //     $this->display();
     }
 
@@ -96,6 +117,7 @@ class ApiController extends ComController
             //可以注册逻辑
             $data['moblie']= $moblie;
             $data['password']= $password;
+            $data['code']= make_coupon_card();//邀请码
             $tjcg=$model->add($data);
             if($tjcg){
                 returnApiSuccess('添加成功',$tjcg);
@@ -116,7 +138,12 @@ class ApiController extends ComController
         $where['sex']=$sex;
         $limit=10;
         $data=xm_gf($table,$field,$where,$limit);
-        returnApiSuccess('请求成功',$data);
+        if($data){
+            returnApiSuccess('请求成功',$data);
+        }else{
+            returnApiError( '请求失败');
+        }
+
     }
 
     //确认选择性别和职业
@@ -135,7 +162,7 @@ class ApiController extends ComController
         $name_obj = new RndChinaName();
         $name = $name_obj->getName(2);
         $where['id'] =  $user_id;
-        $data['sex']=1;
+        $data['sex']= $sex;
         $data['tc_id']= $xzid;
         $data['nm']=  $name;
         $User = M('XmMember')->where($where)->save($data);
@@ -150,18 +177,19 @@ class ApiController extends ComController
     public function xmandage()
     {
         $username=isset($_POST['username']) ? trim($_POST['username']) : '';//姓名
-        $age= intval($_POST['age']) > 0 ?$_POST['age'] : '0';//年纪
-        $user_id = intval($_POST['user_id']) > 0 ?$_POST['user_id'] : '';//用户id
+        $birth= intval($_POST['birth']) > 0 ?intval(trim($_POST['birth'])) : '0';//出生时间搓559238400
+        $user_id = intval($_POST['user_id']) > 0 ?intval($_POST['user_id'])  : '';//用户id
 
         //验证
         if ($username == '') { returnApiError( '姓名必须！');}
-        if ($age == 0) {returnApiError( '年纪必须！');}
+        if ($birth == 0) {returnApiError( '出生时间必须！');}
         if ( $user_id == '') { returnApiError( '用户id必须！');}
 
         //逻辑
         $where['id'] =  $user_id;
         $data['o_username']= $username;
-        $data['age']=  $age;
+        $data['birth']=  $birth;
+        $data['is_news']= 1;
         $User = M('XmMember')->where($where)->save($data);
         if ($User) {
             returnApiSuccess('请求成功', $User);
@@ -196,12 +224,13 @@ class ApiController extends ComController
             M('XmMember')->where($where)->save($data);
 
 
-            $users = $model->field('id,sex,moblie,is_fwz,is_nm,nm,o_username,sessionId')->where(array('moblie' => $moblie, 'password' => $password))->find();
+            $users = $model->field('id,sex,moblie,is_fwz,is_nm,nm,o_username,sessionId,birth')->where(array('moblie' => $moblie, 'password' => $password))->find();
 
             $key =$this->key_xm;
             $token =$users;
             $jwt = JWT::encode($token, $key);
             $data=$users;
+
             $data['token'] =  $jwt;
             returnApiSuccess('登入成功', $data);
             exit(0);
@@ -214,35 +243,303 @@ class ApiController extends ComController
     //首页
     public function syindex()
     {
-        //验证必须登入
-        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
-       $str = isset($_POST['token']) ? trim($_POST['token']) : '';
-       $key =  $this->key_xm;
-
-        if ($user_id == '') { returnApiError( '用户id必须');}
-        if ($str == '') { returnApiError( 'token必须');}
-        if ($key == '') { returnApiError( 'key必须');}
-        $is_jwt=$this->is_jwt($str,$key,1,$user_id);
-
-
-
-
-
+//        //验证必须登入
+//        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+//        $str = isset($_POST['token']) ? trim($_POST['token']) : '';
+//        $key =  $this->key_xm;
+//        if ($user_id == '') { returnApiError( '用户id必须');}
+//        if ($str == '') { returnApiError( 'token必须');}
+//        if ($key == '') { returnApiError( 'key必须');}
+//        $this->is_jwt($str,$key,1,$user_id);
 
         $table="Flash";
         $field='id,pic,tp_sp';
         $where['type']=3;
         $limit=3;
         $data=xm_gf($table,$field,$where,$limit);
-        returnApiSuccess('请求成功',$data);
+        if($data){
+            returnApiSuccess('请求成功',$data);
+        }else{
+            returnApiError('请求失败');
+        }
+     }
+    
+     //---------------------------------------------------------------------个人中心开始--------------------------------------
+    //个人中心
+    public function PCenter()
+    {
+//        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+//        $str = isset($_POST['token']) ? trim($_POST['token']) : '';
+//        $key =  $this->key_xm;
+//        if ($user_id == '') { returnApiError( '用户id必须');}
+//        if ($str == '') { returnApiError( 'token必须');}
+//        if ($key == '') { returnApiError( 'key必须');}
+//        $this->is_jwt($str,$key,1,$user_id);
+
+    }
+
+//  //视频验证
+//    public function PVideoValidation()
+//    {
+//        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+//        $str = isset($_POST['token']) ? trim($_POST['token']) : '';
+//        $key =  $this->key_xm;
+//        if ($user_id == '') { returnApiError( '用户id必须');}
+//        if ($str == '') { returnApiError( 'token必须');}
+//        if ($key == '') { returnApiError( 'key必须');}
+//        $this->is_jwt($str,$key,1,$user_id);
+//    }
+//
+//
+//    //添加约伴
+//    function  addgo(){
+//
+//        $uid = isset($_POST['uid']) ? trim($_POST['uid']) : '';//用户id
+//        $title = isset($_POST['title']) ? htmlentities($_POST['title']) : '';
+//        $content = isset($_POST['content']) ? htmlentities($_POST['content']) : '';
+//        $destination = isset($_POST['destination']) ? htmlentities($_POST['destination']) : '';
+//        $requirement = isset($_POST['requirement']) ? htmlentities($_POST['requirement']) : '';
+//        $cost = isset($_POST['cost']) ? htmlentities($_POST['cost']) : '';
+//        $people_number = isset($_POST['people_number']) ? trim($_POST['people_number']) : '1';
+//        $go_off = isset($_POST['go_off']) ? trim($_POST['go_off']) : '';//
+//        $back_time = isset($_POST['back_time']) ? trim($_POST['back_time']) : '';//
+//        $is_pf = isset($_POST['is_pf']) ? trim($_POST['is_pf']) : '';//
+//        $cfd=isset($_POST['cfd']) ? htmlentities($_POST['cfd']) : '';
+////标签
+//        $bq = isset($_POST['bq']) ? trim($_POST['bq']) : '';//
+//        //   echo $bq;exit;
+//        if($uid){
+//            verifys($_POST['verify'],$uid);
+//
+//            //文件上传地址提交给他，并且上传完成之后返回一个信息，让其写入数据库
+////dump($_FILES);exit;
+//            if(empty($_FILES)){
+//                returnApiError( '必须选择上传文件');
+//
+//            }else{
+//                $a=$this->up();
+//                $xdata['cfd']=$cfd;
+//                $xdata['title']=$title;
+//                $xdata['user_id']=$uid;
+//                $xdata['content']=$content;
+//                $xdata['destination']=$destination;
+//                $xdata['requirement']=$requirement;
+//                $xdata['cost']=$cost;
+//                $xdata['people_number']=$people_number;
+//                $xdata['go_off']=$go_off;
+//                $xdata['back_time']=$back_time;
+//                $xdata['is_pf']=$is_pf;
+//                $xdata['t']=time();
+//
+//                if( $bq){
+//                    $bqs = array_filter(explode('|',$bq));
+//
+//                }
+//
+//                if(isset($a)){
+//                    //写入数据库的自定义c方法
+//                    if($this->c($a,$xdata,$bqs)){
+//                        returnApiSuccess('1','上传成功');
+//
+//                    }
+//                    else{
+//                        returnApiError( '写入数据库失败');
+//
+//                    }
+//                }else{
+//                    returnApiError( '上传文件异常，请与系统管理员联系');
+//
+//                }
+//            }
+//
+//        }else{
+//            returnApiError( '请登入');
+//        }
+//
+//    }
+//
+////接收文件
+//    private function c($data,$xdata=null,$pdata=null){
+//        $prefix = C('DB_PREFIX');
+//        foreach($data as $k=>$file){
+//            $img_val='/file/'.$file['savepath'].$file['savename'];
+//            if($k<7){
+//                $ks=$k+1;
+//                //$datas["img$ks"] ='/file/'.$file['savepath'].$file['savename'];
+//                $datas["img$ks"] =$img_val;
+//            }
+//        }
+//        $datas = array_merge($datas,$xdata);
+//        $file=M('go');
+//        if($file->add($datas)){
+//            if($pdata){
+//
+//                foreach($pdata as $k=>$value){
+//                    $pd['sid']=1;
+//                    $pd['table_name']= "{$prefix}member";
+//                    $pd['bid']=$xdata['user_id'];
+//                    $pd['tab_id']=$value;
+//                    $pd['t']=time();
+//                    M('tab_nexus')->add($pd);
+//                }
+//            }
+//
+//            return true;
+//        }else{
+//            return false;
+//        }
+//
+//    }
+//    function up(){
+//        $upload = new \Think\Upload();// 实例化上传类
+//        $upload->maxSize   =     3145728 ;// 设置附件上传大小
+//        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+//        $upload->rootPath  =      './file/'; // 设置附件上传根目录
+//        $upload->savePath  =      ''; // 设置附件上传（子）目录
+//// 上传文件
+//        $info   =   $upload->upload();
+//        if(!$info) {// 上传错误提示错误信息
+//            $this->error($upload->getError());
+//        }else{// 上传成功 获取上传文件信息
+//            return $info;
+//
+//        }
+//
+//    }
+    
+    //---------------------------------------------------------------------个人中心结束--------------------------------------
+
+    //-----------------------------------------------------------------男性模块开始-------------------------------------------
+    //约会主题
+    public function ManDateTheme()
+    {
+        //验证必须登入
+//        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+//        $str = isset($_POST['token']) ? trim($_POST['token']) : '';
+//        $key =  $this->key_xm;
+//        if ($user_id == '') { returnApiError( '用户id必须');}
+//        if ($str == '') { returnApiError( 'token必须');}
+//        if ($key == '') { returnApiError( 'key必须');}
+//        $this->is_jwt($str,$key,1,$user_id);
+
+        $table="XmTrystClassify";
+        $field='*';
+        $data=xm_gf($table,$field);
+//数据添加图片宽高
+
+        foreach( $data as $key=>$value){
+           if($value['id']==1){
+               $value[w] = 320;
+               $value[h] = 360;
+           }else{
+               $value[w] = 320;
+               $value[h] = 200;
+           }
+            $datas[]= $value;
+        }
+
+        if($data){
+            returnApiSuccess('请求成功',$datas);
+        }else{
+            returnApiError('请求失败');
+        }
+    }
+
+//。。。。。。。-----------------------------------------------------------------男性线下模块开始-------------------------------------------
+//发约订单生成
+    public function ManTrystCreate()
+    {
+        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+        $classify= intval($_POST['classify']) > 0 ?intval(trim($_POST['classify'])) : '0';//分类id
+        $createtime= intval($_POST['createtime']) > 0 ?intval(trim($_POST['createtime'])) : '0';//约会时间
+        $time_limit= intval($_POST['time_limit']) > 0 ?intval(trim($_POST['time_limit'])) : '0';//约会时限
+        $appointment_dd=isset($_POST['appointment_dd']) ? trim($_POST['appointment_dd']) : '';//约会地点
+        $is_sex= intval($_POST['is_sex']) > 0 ?'1' : '0';//要求性别
+        $people_num= intval($_POST['people_num']) > 0 ?intval(trim($_POST['people_num'])) : '1';//要求人数
+        $is_drink= intval($_POST['is_drink']) > 0 ?'1' : '0';//是否喝酒
+        $is_anonymous= intval($_POST['is_anonymous']) > 0 ?intval(trim($_POST['is_anonymous'])) : '0';//是否匿名
+        $remarks=isset($_POST['remarks']) ? trim($_POST['remarks']) : ''; //备注
+        $money= intval($_POST['money']) > 0 ?intval(trim($_POST['money'])) : '0';//金额
+        $daz_business_id= intval($_POST['daz_business_id']) > 0 ?intval(trim($_POST['daz_business_id'])) : '0';//大众点评id
+        $target_area = isset($_POST['target_area']) ? trim($_POST['target_area']) : '';//地点
+
+        //过滤
+        if($user_id==""){ returnApiError( '用户id必须');}
+        if($createtime==0){ returnApiError( '约会时间必须');}
+        if($time_limit==0){ returnApiError( '约会时限必须');}
+        if($appointment_dd==""){ returnApiError( '约会地点必须');}
+        if($money==0){ returnApiError( '金额必须');}
+        //逻辑
+         $data['order_number']= build_order_no(); //订单编号
+         $data['user_id']= $user_id;//发起者用户id
+         $data['classify']= $classify;//分类id
+         $data['appointment_time']=  $createtime;//约会时间
+         $data['time_limit']= $time_limit;//约会时限
+         $data['appointment_dd']= $appointment_dd;//约会地点
+         $data['is_sex']=  $is_sex;//要求性别
+         $data['people_num']=  $people_num;//要求人数
+         $data['is_drink']=  $is_drink;//是否喝酒
+         $data['is_anonymous']=  $is_anonymous;//是否匿名
+         $data['remarks']=  $remarks;//备注
+         $data['money']=  $money;//金额
+         $data['status']=  0;
+         $data['daz_business_id']= $daz_business_id;
+         $data['target_area']=  $target_area;
+         $data['time_add']=  time();
+         $model=M('XmOrder');
+         $tjcg=$model->add($data);
+        if($tjcg){
+            returnApiSuccess('添加成功',$tjcg);
+        }else{
+            returnApiError( '添加失败');
+        }
 }
+
+    //支付选择
+    public function ManPaymentChoice()
+    {
+        $table="XmPayment";
+        $field='id,title,option';
+        $where['is_enable']=1;
+        $data=xm_gf($table,$field,$where);
+        foreach( $data as $value){
+            $value['option']=explode(',',$value['option']);
+            $datas[]= $value;
+        }
+        if($data){
+            returnApiSuccess('查询成功',$datas);
+        }else{
+            returnApiError( '查询失败');
+        }
+
+}
+ //支付前审核
+    public function ManPaymentExamine()
+    {
+        $user_id = isset($_POST['user_id']) ? trim($_POST['user_id']) : '';//用户id
+        $order_id = isset($_POST['order_id']) ? trim($_POST['order_id']) : '';//订单id
+        $f_money= intval($_POST['money']) > 0 ?intval(trim($_POST['money'])) : '0';//金额
+        $User = M("XmOrder"); // 实例化User对象
+        $where['order_id']= $order_id;
+        $data = $User->where($where)->find();
+       print_r($data);
+        //订单和要好支付的状态
+        //支付金额
+        //用户余额
+    }
+
+//。。。。。。。-----------------------------------------------------------------男性线下模块结束-------------------------------------------
+    //-----------------------------------------------------------------男性模块结束-------------------------------------------
+
+
+
 
 
 
 //jwt测试
 //    public function one(){
 //
-//        $key = "xmkly";
+//        $key =  $this->key_xm;
 //        $token = array(
 //            'uid' => 1050,
 //            'username' => 'baby',
@@ -253,6 +550,7 @@ class ApiController extends ComController
 //    }
 //    public function two(){
 //        $key = "xmkly";
+//        $key =  $this->key_xm;
 //        $str = isset($_POST['str']) ? $_POST['str'] : '';
 //        if($str == ''){
 //            exit('empty');
